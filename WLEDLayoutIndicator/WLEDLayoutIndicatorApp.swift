fdd00873d91bd4d46eac5967e76613365a39bfe7
Sergei Grieg <s.v.grigoryev@gmail.com>
2026-04-11 22:00:57 +0200

    Initial Commit

---
+++ WLEDLayoutIndicator/WLEDLayoutIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WLEDLayoutIndicatorApp.swift
+//  WLEDLayoutIndicator
+//
+//  Created by Sergei Grieg on 11.04.26.
+//
+
+import SwiftUI
+
+@main
+struct WLEDLayoutIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WLEDLayoutIndicatorTests/WLEDLayoutIndicatorTests.swift
@@ -0,0 +1,19 @@
+//
+//  WLEDLayoutIndicatorTests.swift
+//  WLEDLayoutIndicatorTests
+//
+//  Created by Sergei Grieg on 11.04.26.
+//
+
+import Testing
+@testable import WLEDLayoutIndicator
+
+struct WLEDLayoutIndicatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
